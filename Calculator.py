721eeb313e5a704bc699666fa299f3f2bb60c858
preston040809824 <preston.deriger@gmail.com>
2024-06-08 16:50:47 -0400

    init commit

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+first_number = float(input("first number: "))
+second_number = float(input("second number: "))
+
+sum = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+
+print("Sum is "+str(sum))
+print("subtraction is "+str(subtraction))
+print("multiplication is "+str(multiplication))
+print("division is "+str(division))
